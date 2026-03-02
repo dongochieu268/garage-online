ef934a9eb1eb162dddccf124aa9a44ab4bdd3069
Do Ngoc Hieu <dongochieu268@gmail.com>
2026-03-02 21:51:43 +0700

    create service model

---
+++ src/java/model/Service.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author ADMIN
+ */
+public class Service {
+    private int id;
+    private String name;
+    private double decimal;
+    private String description;
+    
+    
+}
