bd303cd1213c4cd0341dec4f881a2e8703251810
Do Ngoc Hieu <dongochieu268@gmail.com>
2026-03-14 21:05:50 +0700

    crud service for admin

---
+++ src/java/controller/UdateServiceController.java
@@ -0,0 +1,109 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+
+package controller;
+
+import dal.serviceDAO;
+import java.io.IOException;
+import java.io.PrintWriter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import model.Service;
+
+/**
+ *
+ * @author ADMIN
+ */
+@WebServlet(name="updateServiceController", urlPatterns={"/updateService"})
+public class UdateServiceController extends HttpServlet {
+   
+    /** 
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {              
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet updateServiceController</title>");  
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet updateServiceController at " + request.getContextPath () + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    } 
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /** 
+     * Handles the HTTP <code>GET</code> method.
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+        String id_raw = request.getParameter("id");
+        try{
+            int id = Integer.parseInt(id_raw);
+            serviceDAO dao = new serviceDAO();
+            Service s = dao.getById(id);
+            request.setAttribute("service", s);
+            request.getRequestDispatcher("/views/admin/service/update.jsp").forward(request, response);
+        }catch(Exception e){
+            
+        }
+        request.getRequestDispatcher("/views/admin/service/update.jsp").forward(request, response);
+    } 
+
+    /** 
+     * Handles the HTTP <code>POST</code> method.
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+        String id_raw = request.getParameter("id");
+        String name = request.getParameter("name");
+        String price_raw = request.getParameter("price");
+        String description = request.getParameter("description");
+        try{
+            int id = Integer.parseInt(id_raw);
+            double price = Double.parseDouble(price_raw);
+            Service s = new Service(id, name, price, description);
+            serviceDAO dao = new serviceDAO();
+            dao.update(s);
+            
+        }catch(Exception e){
+            
+        }
+        response.sendRedirect("service");
+    }
+
+    /** 
+     * Returns a short description of the servlet.
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
