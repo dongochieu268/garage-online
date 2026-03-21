e8fb4e031e33c573e2314062fb22fd7b25369b60
Do Ngoc Hieu <dongochieu268@gmail.com>
2026-03-21 18:48:20 +0700

    update

--- src/java/controller/UdateServiceController.java
+++ src/java/controller/UdateServiceController.java
@@ -1,14 +1,19 @@
 package controller;
 
+import dal.CategoryDAO;
 import dal.serviceDAO;
+import java.io.File;
 import java.io.IOException;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.MultipartConfig;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.Part;
 import model.Service;
 
+@MultipartConfig
 @WebServlet(name = "updateServiceController", urlPatterns = {"/admin/updateService"})
 public class UdateServiceController extends HttpServlet {
 
@@ -20,6 +25,10 @@ public class UdateServiceController extends HttpServlet {
             int id = Integer.parseInt(id_raw);
             serviceDAO dao = new serviceDAO();
             Service s = dao.getById(id);
+
+            CategoryDAO cdao = new CategoryDAO();
+            request.setAttribute("categories", cdao.getAll());
+
             request.setAttribute("service", s);
             request.getRequestDispatcher("/views/admin/service/update.jsp").forward(request, response);
             return;
@@ -36,13 +45,29 @@ public class UdateServiceController extends HttpServlet {
         String name = request.getParameter("name");
         String price_raw = request.getParameter("price");
         String description = request.getParameter("description");
-        String img = request.getParameter("img");
+        String oldImg = request.getParameter("oldImg");
         String categoryId_raw = request.getParameter("categoryId");
 
         try {
             int id = Integer.parseInt(id_raw);
             double price = Double.parseDouble(price_raw);
             int categoryId = Integer.parseInt(categoryId_raw);
+
+            String img = oldImg;
+
+            Part filePart = request.getPart("image");
+            String fileName = filePart.getSubmittedFileName();
+
+            if (fileName != null && !fileName.isEmpty()) {
+                String uploadPath = getServletContext().getRealPath("/images/services");
+                File dir = new File(uploadPath);
+                if (!dir.exists()) {
+                    dir.mkdirs();
+                }
+
+                filePart.write(uploadPath + File.separator + fileName);
+                img = "images/services/" + fileName;
+            }
 
             Service s = new Service();
             s.setId(id);
@@ -53,12 +78,26 @@ public class UdateServiceController extends HttpServlet {
             s.setCategoryId(categoryId);
 
             serviceDAO dao = new serviceDAO();
-            dao.update(s);
+            boolean check = dao.update(s);
+
+            if (check) {
+                response.sendRedirect(request.getContextPath() + "/admin/service");
+                return;
+            } else {
+                CategoryDAO cdao = new CategoryDAO();
+                request.setAttribute("categories", cdao.getAll());
+                request.setAttribute("error", "Can not update");
+                request.setAttribute("service", s);
+                request.getRequestDispatcher("/views/admin/service/update.jsp").forward(request, response);
+                return;
+            }
 
         } catch (Exception e) {
+            CategoryDAO cdao = new CategoryDAO();
+            request.setAttribute("categories", cdao.getAll());
+            request.setAttribute("error", "Can not update");
+            request.getRequestDispatcher("/views/admin/service/update.jsp").forward(request, response);
         }
-
-        response.sendRedirect(request.getContextPath() + "/admin/service");
     }
 
     @Override
